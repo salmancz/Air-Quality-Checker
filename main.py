469bfca58cac30e2792e417354915bacecf5a99a
Siva Ramana H V <94345985+SivaRamana-H-V@users.noreply.github.com>
2022-04-01 09:47:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # HackOverFlow - Team QUINN
 import streamlit as st
 from datetime import date
+import Rpi.GPIO as GPIO
+#Raspberry pi gpio numbers
+SPICLK=
+SPIMISO=
+SPICS=
+SPIMOSI=
+#sensor pin numbers
