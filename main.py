8cc77118e8b6824579758660c327f72568b25529
Siva Ramana H V <94345985+SivaRamana-H-V@users.noreply.github.com>
2022-04-01 12:24:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,36 @@ mq135_apin=
 def init():
     GPIO.setwarnings(False)
     GPIO.cleanup()		
-    GPIO.setmode(GPIO.BCM)		
+    GPIO.setmode(GPIO.BCM)
+#set up the SPI interface pins
+    GPIO.setup(SPIMOSI, GPIO.OUT)
+    GPIO.setup(SPIMISO, GPIO.IN)
+    GPIO.setup(SPICLK, GPIO.OUT)
+    GPIO.setup(SPICS, GPIO.OUT)
+    GPIO.setup(mq135_dpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+#Read SPI data from MCP3008
+def readadc(adcnum, clockpin, mosipin, misopin, cspin:
+	if ((adcnum > 7) or (adcnum < 0)):
+		return -1
+		GPIO.output(cspin, True)	
+
+		GPIO.output(clockpin, False) 
+		GPIO.output(cspin, False) 
+		commandout = adcnum
+		commandout |= 0x18  
+		commandout <<= 3    
+		for i in range(5):
+			if (commandout & 0x80):
+				GPIO.output(mosipin, True)
+			else:
+				GPIO.output(mosipin, False)
+				commandout <<= 1
+				GPIO.output(clockpin, True)
+				GPIO.output(clockpin, False)
+
+		adcout = 0
+
+
 # st.markdown("<script src='https://kit.fontawesome.com/ad34c27ecf.js' crossorigin='anonymous'></script>")
 st.set_page_config(layout="wide" , page_icon="💬" , page_title="Air Quality Checker app")
 st.markdown("<h2 style='text-align: center; color: #ccc; margin-top : -85px; margin-bottom: 20px'>Air Quality Checker</h2>", unsafe_allow_html=True)
