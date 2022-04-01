ccdd9ab39597789dfbfbb9f5521b5884aa88067b
Salman <salman.jsdev@gmail.com>
2022-04-01 15:22:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 import streamlit as st
 from datetime import date
 import Rpi.GPIO as GPIO
+
+
 #Raspberry pi gpio numbers
 SPICLK=
 SPIMISO=
 SPICS=
 SPIMOSI=
 #sensor pin numbers
+
+
+# st.markdown("<script src='https://kit.fontawesome.com/ad34c27ecf.js' crossorigin='anonymous'></script>")
+st.set_page_config(layout="wide" , page_icon="💬" , page_title="Air Quality Checker app")
+
+today = date.today()
+st.markdown("<h2 style='text-align: center; color: #ccc; margin-top : -85px; margin-bottom: 20px'>Air Quality Checker</h2>", unsafe_allow_html=True)
